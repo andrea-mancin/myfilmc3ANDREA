21611eca8e975ce4159c8bf3abc57754bc78498d
Andrea <kustomx@hotmail.it>
2020-11-16 10:02:59 +0100

    Project base

---
+++ myfilmc3ANDREA/src/main/java/it/course/exam/myfilmc3ANDREA/Myfilmc3AndreaApplication.java
@@ -0,0 +1,13 @@
+package it.course.exam.myfilmc3ANDREA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myfilmc3AndreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myfilmc3AndreaApplication.class, args);
+	}
+
+}
---
+++ myfilmc3ANDREA/src/test/java/it/course/exam/myfilmc3ANDREA/Myfilmc3AndreaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.course.exam.myfilmc3ANDREA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myfilmc3AndreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
